397584dd7e42e50217f6e4032a808b652cdc5660
EstebanSalazar-27 <estebanenis27@gmail.com>
2024-05-23 10:07:55 -0300

    chat icon

--- src/components/TableComponents/td.tsx
+++ src/components/TableComponents/td.tsx
@@ -3,13 +3,16 @@ import { ReactNode } from "react";
 export const TD = ({
  children,
  title,
+ onClick,
 }: {
  children: ReactNode;
  title?: string;
+ onClick?: () => void;
 }) => {
  const EMPTYCELL = "-------";
  return (
   <td
+   onClick={onClick}
    title={title}
    className=" whitespace-nowrap overflow-hidden overflow-ellipsis lowercase first-letter:capitalize    py-2 text-center text-xs max-w-[150px]  border-x-[1px]  dark:border-stone-700 dark:text-slate-200"
   >
--- src/components/filterBar/index.tsx
+++ src/components/filterBar/index.tsx
@@ -28,56 +28,58 @@ function FiltersBar({
 }) {
  return (
   <aside className="h-full w-max min-w-[200px] rounded-sm flex flex-col gap-4 border-t-2 ">
-   <FilterCont>
-    <Label fieldName="client">
-     Bots
-     <select
-      title="Seleccionar bot"
-      name="bot"
-      id="bot"
-      className=" w-full  text-sm bg-white dark:bg-stone-800 dark:text-slate-200 border border-gray-300 hover:border-blue-600  p-2 rounded-sm shadow-sm font-semibold transition text-light-D focus:outline-none focus:shadow-outline"
-      value={filters.bot}
-      onChange={handleFilters}
-     >
-      <option value="0" disabled selected>
-       Seleccionar bot
-      </option>
-      {bots.map((client) => (
-       <option value={client.id} key={client.id}>
-        {client.name}
+   <form>
+    <FilterCont>
+     <Label fieldName="client">
+      Bots
+      <select
+       title="Seleccionar bot"
+       name="bot"
+       id="bot"
+       className=" w-full  text-sm bg-white dark:bg-stone-800 dark:text-slate-200 border border-gray-300 hover:border-blue-600  p-2 rounded-sm shadow-sm font-semibold transition text-light-D focus:outline-none focus:shadow-outline"
+       value={filters.bot}
+       onChange={handleFilters}
+      >
+       <option value="0" disabled selected>
+        Seleccionar bot
        </option>
-      ))}
-     </select>
-    </Label>
-   </FilterCont>
-
-   <FilterCont>
-    <Label fieldName="fromDate">Rango de fecha</Label>
-
-    <div className="flex flex-col gap-2 ">
-     <Label fieldName="fromDate">
-      Desde
-      <DateInput
-       max={filters.local_updated__date__lte}
-       onChange={handleFilters}
-       value={filters.local_updated__date__gte}
-       fieldName="local_updated__date__gte"
-      />
+       {bots.map((client) => (
+        <option value={client.id} key={client.id}>
+         {client.name}
+        </option>
+       ))}
+      </select>
      </Label>
+    </FilterCont>
 
-     <Label fieldName="toDate">
-      Hasta
-      <DateInput
-       min={filters.local_updated__date__gte}
-       onChange={handleFilters}
-       value={filters.local_updated__date__lte}
-       fieldName="local_updated__date__lte"
-      />
-     </Label>
-    </div>
-   </FilterCont>
+    <FilterCont>
+     <Label fieldName="fromDate">Rango de fecha</Label>
 
-   <Button onClick={cleanFilters}>Limpiar filtros</Button>
+     <div className="flex flex-col gap-2 ">
+      <Label fieldName="fromDate">
+       Desde
+       <DateInput
+        max={filters.local_updated__date__lte}
+        onChange={handleFilters}
+        value={filters.local_updated__date__gte}
+        fieldName="local_updated__date__gte"
+       />
+      </Label>
+
+      <Label fieldName="toDate">
+       Hasta
+       <DateInput
+        min={filters.local_updated__date__gte}
+        onChange={handleFilters}
+        value={filters.local_updated__date__lte}
+        fieldName="local_updated__date__lte"
+       />
+      </Label>
+     </div>
+    </FilterCont>
+
+    <Button onClick={cleanFilters}>Limpiar filtros</Button>
+   </form>
   </aside>
  );
 }
